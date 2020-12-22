c770dee7fa68eecf394879d03f54711f473b240e
Yuvraj singh <yuvrajthakur292@gmail.com>
2020-12-22 15:28:08 +0530

    PR7 changes in server routes

---
+++ server/connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+
+
+var mysqlConnnection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database:"hoteldb",
+    multipleStatements: true
+  });
+  
+  mysqlConnnection.connect(function(err) {
+    if (err) throw err
+    else{
+        console.log("Connected!");
+    }
+   
+  });
+
+
+  module.exports = mysqlConnnection;
---
+++ server/routes/user.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const mysqlConnnection = require("../connection")
+const router = express.Router();
+
+router.get("/",function(req, res){
+    mysqlConnnection.query("SELECT * FROM hotel WHERE HOTEL_ID=?",[1],function(err,rows,field){
+        if(err){
+            console.log(err)
+        }
+        else{
+           console.log(rows)
+            res.send(rows);
+        }
+    })
+    
+        
+    })
+
+    router.post("/user/location", function (req, res) {
+        mysqlConnnection.query(
+          "SELECT ? FROM hotel WHERE HOTEL_ID=?",
+          ["HOTEL_NAME", 1],
+          function (err, rows, field) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.send(rows);
+            }
+          }
+        );
+      });
+
+module.exports =router;
